e83c439c263daaf48175ecb0303c237a06965a9b
Aleksi <aleksipekkala@hotmail.com>
2013-08-13 15:54:55 +0300

    Osaa selvittaa transformaatiosaannot

--- tagger.hs
+++ tagger.hs
@@ -5,6 +5,7 @@ import qualified Data.List as L
 import qualified Data.Map  as M
 import qualified Data.Set as S
 import qualified Data.List.Zipper as Z
+import Control.Monad (forM)
 
 type Token = String
 type Tag = String
@@ -34,6 +35,16 @@ main = do
     putStrLn $ "oikein:         " ++ show c
     putStrLn $ "ei tunnistettu: " ++ show u
     putStrLn $ show (100 * fromIntegral c / fromIntegral n) ++ " %"
+
+-- Selvitetään kymmenen yleisintä transformaatiosääntöä
+main' :: IO [()]
+main' = do
+  putStrLn "Selvitetään 10 yleisintä transformaatiosääntöä..."
+  ls <- fmap initialLearningState readTrainingInstances
+  let ruleMap = instRules ruleInstantiators ls
+  let sorted = L.sortBy (\(_,a) (_,b) -> compare b a) $ M.toList ruleMap
+  forM (take 10 sorted) $ \(ts, count) -> do
+      putStrLn $ show count ++ "kpl: " ++ show ts
 
 
 
@@ -74,6 +85,11 @@ readFreqTagMap = fmap trainFreqTagger readTrainingInstances
 
 freqTag :: M.Map Token Tag -> Token -> Maybe Tag
 freqTag m t = M.lookup t m
+
+backupTag :: (Token -> Maybe Tag) -> Tag -> Token -> Tag
+backupTag tagger backup token = case tagger token of
+                                  Nothing -> backup
+                                  Just t  -> t
 
 evalTagger :: (Token -> Maybe Tag) -> [TrainingInstance] -> (Int, Int, Int)
 evalTagger tagger = L.foldl' eval (0, 0, 0)
@@ -123,12 +139,34 @@ instRules' :: [(Z.Zipper (Tag, Tag) -> Maybe TransformationRule)] ->
 instRules' fs = Z.foldlz' getRules S.empty
   where
     getRules acc zipper
-      | correct == incorrect = acc
+      | correct == incorrect = acc -- taggeri oli oikeassa, ei poimita korvaussääntöä
       | otherwise = foldl (applyFunc zipper) acc fs
-      where (correct, incorrect) = Z.cursor zipper
-    applyFunc z acc f = case f z of
-                          Nothing -> acc
-                          Just r  -> S.insert r acc
+      where
+        (correct, incorrect) = Z.cursor zipper
+        applyFunc z acc f = case f z of
+                              Nothing -> acc
+                              Just r  -> S.insert r acc
+
+instRules :: [(Z.Zipper (Tag, Tag) -> Maybe TransformationRule)] ->
+              Z.Zipper (Tag, Tag) -> M.Map TransformationRule Int
+instRules fs = Z.foldlz' getRules M.empty
+  where
+    getRules acc zipper
+      | correct == incorrect = acc -- taggeri oli oikeassa, ei poimita korvaussääntöä
+      | otherwise = foldl (applyFunc zipper) acc fs
+      where
+        (correct, incorrect) = Z.cursor zipper
+        applyFunc z acc f = case f z of
+                              Nothing -> acc
+                              Just r  -> M.insertWith (+) r 1 acc
+
+-- "sana/sanaluokka"-listasta Zipper, jossa alkioina (oikea sanaluokka, arvattu sanaluokka)
+initialLearningState :: [TrainingInstance] -> Z.Zipper (Tag, Tag)
+initialLearningState ts = Z.fromList $ map toTagPair ts
+  where
+    toTagPair (TrainingInstance token tag) = (tag, tagFunc token)
+    tagFunc = backupTag (freqTag (trainFreqTagger ts)) "NN"
+
 
 
 taggingState = Z.fromList $ zip ["AT","NN","TO"] ["AT","VB","TO"]
